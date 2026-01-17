e8daaed9ad623ad9cfe39f8442a12e85187f23a1
Oscar Soirey <47278608+oscar-soirey@users.noreply.github.com>
2026-01-17 15:05:25 +0000

    Delete cmake-build-debug-mingw directory

--- cmake-build-debug-mingw/generated/gl_ui_frag_shader.cpp
+++
@@ -1,5 +0,0 @@
-const unsigned char gl_ui_frag_shader[] = {
-
-};
-const unsigned int gl_ui_frag_shader_len = 0;
-
--- cmake-build-debug-mingw/generated/gl_ui_vert_shader.cpp
+++
@@ -1,5 +0,0 @@
-const unsigned char gl_ui_vert_shader[] = {
-
-};
-const unsigned int gl_ui_vert_shader_len = 0;
-
